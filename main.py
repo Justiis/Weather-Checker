0672aaa04da9fe612f928a19ebc4efc682c76161
Big Head <52704158+internall@users.noreply.github.com>
2021-01-07 19:39:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import requests
 
 api = "http://api.openweathermap.org/data/2.5/weather?appid=bsos0e0f375c&q="
 
-'''WINDOW'''
+#creating a tk window
 window = Tk(); window.title('Weather Checker'); window.geometry('400x200+650+300'); window.resizable(0, 0);
 city = StringVar()
-'''window'''
-
 lab = Label(window, text = 'Enter the name of the city: ', font = 'CGTimes', fg = 'BLACK').grid(row=1, column = 0)
 entry = Entry(window, textvariable = city).grid(row=1, column= 3)
 labelCity = Label(window, )
+
 def createWeather():
-    '''API'''
+    '''parse api data and place them inside the labels'''
     json_data = requests.get(api + str(city.get())).json()
     temp = "{0:.2f}".format(json_data['main']['temp'] - 273.15)
     statement = str(json_data['weather'][0]['main'])
     humidity = str(json_data['main']['humidity'])
-    '''api'''
+    
     labelCity = Label(window, text = '\n\nCity: ' + city.get(),font = 'Charter', fg = 'BROWN').grid(row = 2, column = 0)
     labelTemp = Label(window, text='Temperature: ' + str(temp), font = 'Charter', fg = 'red').grid(row=3, column=0)
     labelSatement = Label(window, text = 'Statement: ' + statement ,font = 'Charter', fg = 'green').grid(row = 4, column = 0)
